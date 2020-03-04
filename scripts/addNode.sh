c5d65eacfec0532bfd8d0c9a0972dce52213d2ab
Vitaliy Moiseev <itspec144@gmail.com>
2020-03-04 15:43:05 +0300

    Update addNode.sh

--- scripts/addNode.sh
+++ scripts/addNode.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-tsm_admin_user = $1
-tsm_admin_pass = $2
-primary_dns = $3
-INSTALL_SCRIPT_URL = $4
+tsm_admin_user=$1
+tsm_admin_pass=$2
+primary_dns=$3
+INSTALL_SCRIPT_URL=$4
+
+yum -y install sshpass
 
 cd /tmp
-expect -c \"spawn sftp -o \\\"StrictHostKeyChecking no\\\" \\\"$tsm_admin_user@$primary_dns\\\";expect \\\"password:\\\";send \\\"$tsm_admin_pass\\n\\\";expect \\\"sftp>\\\";send \\\"get bootstrap.cfg\\n\\\";expect \\\"sftp>\\\";send \\\"exit\\n\\\";interact\"
+
+sshpass -p "${tsm_admin_pass}" scp -v -o StrictHostKeyChecking=no $tsm_admin_user@$primary_dns:./bootstrap.cfg /tmp/
+
 
 # download distr
-wget --tries=3 --output-document=tableau-installer.rpm https://downloads.tableau.com/esdalt/2019.4.0/tableau-server-2019-4-0.x86_64.rpm
+wget --tries=3 --output-document=tableau-installer.rpm https://downloads.tableau.com/esdalt/2020.1.0/tableau-server-2020-1-0.x86_64.rpm
 
 # download automated-installer
 wget --remote-encoding=UTF-8 --output-document=automated-installer.sh $INSTALL_SCRIPT_URL
 chmod +x automated-installer.sh
 #installation
 sudo ./automated-installer.sh -a $tsm_admin_user -f /dev/zero -r /dev/zero -b bootstrap.cfg -v --accepteula --force /tmp/tableau-installer.rpm
-firewall-cmd --zone=public --add-port=80/tcp --permanent
-firewall-cmd --reload
