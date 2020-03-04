c1be8a4b566bb90a46cf4cc974b51990f06a5006
Vitaliy Moiseev <itspec144@gmail.com>
2020-03-04 15:51:09 +0300

    Update addNode.sh

--- scripts/addNode.sh
+++ scripts/addNode.sh
@@ -10,6 +10,8 @@ yum -y install sshpass
 cd /tmp
 
 sshpass -p "${tsm_admin_pass}" scp -v -o StrictHostKeyChecking=no $tsm_admin_user@$primary_dns:./bootstrap.cfg /tmp/
+sshpass -p "${tsm_admin_pass}" scp -v -o StrictHostKeyChecking=no $tsm_admin_user@$primary_dns:./secrets /tmp/
+secrets
 
 
 # download distr
@@ -19,4 +21,4 @@ wget --tries=3 --output-document=tableau-installer.rpm https://downloads.tableau
 wget --remote-encoding=UTF-8 --output-document=automated-installer.sh $INSTALL_SCRIPT_URL
 chmod +x automated-installer.sh
 #installation
-sudo ./automated-installer.sh -a $tsm_admin_user -f /dev/zero -r /dev/zero -b bootstrap.cfg -v --accepteula --force /tmp/tableau-installer.rpm
+sudo ./automated-installer.sh -a $tsm_admin_user -f /dev/zero -r /dev/zero -b bootstrap.cfg  -s secrets -v --accepteula --force /tmp/tableau-installer.rpm
